7040b3c82d1f1dc67bec9537ee844e284121cb63
Dmytro Chobotar <dchobotar.fl@gmail.com>
2025-03-10 21:06:04 -0400

    Comments for loops

--- control_flow/loops_in_python.py
+++ control_flow/loops_in_python.py
@@ -1,23 +1,26 @@
 # Loops
-
+# Looping over a List
 fruits = ["apple", "banana", "cherry"]
 
 for fruit in fruits:
     print(f"I like {fruit}s")
     
+# looping Over a String    
 text = "python"
 for letter in text:
     print(letter, end="-")    
 print()
 
+# looping over a range of numbers
 for i in range(10): 
     print(i, end=" ")
 print()
 
+# loopint over a list by using index and element value
 for i, fruit in enumerate(fruits): 
     print(f"{i + 1}. {fruit.capitalize()}")
     
-    
+# looping over a string with index to control the logic    
 text = "python"
 for i, letter in enumerate(text):
     if i == len(text) - 1:
